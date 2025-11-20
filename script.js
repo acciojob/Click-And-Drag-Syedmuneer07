49a42041b32c0bbf8370a9510bfa378c8ada50a9
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2025-11-20 19:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-// Your code here.
- const slider=document.querySelector('.items');
-	let isDown=false;
-let startX;
-let scrollLeft;
+// // Your code here.
+//  const slider=document.querySelector('.items');
+// 	let isDown=false;
+// let startX;
+// let scrollLeft;
 
-slider.addEventListener('mousedown',(e)=>{
-	e.preventDefault();
-	isDown=true;
+// slider.addEventListener('mousedown',(e)=>{
+// 	e.preventDefault();
+// 	isDown=true;
 	
-	slider.classList.add('active');
-	startX=e.pageX - slider.offsetLeft;
-	scrollLeft=slider.scrollLeft;
-});
+// 	slider.classList.add('active');
+// 	startX=e.pageX - slider.offsetLeft;
+// 	scrollLeft=slider.scrollLeft;
+// });
 
-slider.addEventListener('mouseleave',()=>{
-	isDown=false;
-	slider.classList.remove('active');
-});
+// slider.addEventListener('mouseleave',()=>{
+// 	isDown=false;
+// 	slider.classList.remove('active');
+// });
 
-slider.addEventListener('mouseup',()=>{
-	isDown=false;
-	slider.classList.remove('active');
-});
+// slider.addEventListener('mouseup',()=>{
+// 	isDown=false;
+// 	slider.classList.remove('active');
+// });
 
-slider.addEventListener('mousemove',(e)=>{
-	if(!isDown) return;
-	e.preventDefault();
-	const x=e.pageX-slider.offsetLeft;
-	const walk=(x-startX)*2;
-	slider.scrollLeft=scrollLeft-walk;
+// slider.addEventListener('mousemove',(e)=>{
+// 	if(!isDown) return;
+// 	e.preventDefault();
+// 	const x=e.pageX-slider.offsetLeft;
+// 	const walk=(x-startX)*2;
+// 	slider.scrollLeft=scrollLeft-walk;
 	
-});
+// });
 
 
 
