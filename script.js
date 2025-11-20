8c51b97449eef604fe1ea1c8b68637cf9bb392db
Syed muneerul zakireen <syedmuneerzakk@gmail.com>
2025-11-20 19:47:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
-const container = document.querySelector('.items');
-const cubes = document.querySelectorAll('.item');
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
 
-let activeCube = null;
-let offsetX = 0;
-let offsetY = 0;
-
-cubes.forEach(cube => {
-  cube.style.position = "absolute";
-
-  cube.addEventListener("mousedown", (e) => {
-    activeCube = cube;
-    offsetX = e.clientX - cube.offsetLeft;
-    offsetY = e.clientY - cube.offsetTop;
-
-    cube.style.zIndex = 9999;
-  });
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
 
-document.addEventListener("mousemove", (e) => {
-  if (!activeCube) return;
-
-  let x = e.clientX - offsetX;
-  let y = e.clientY - offsetY;
-
-  // boundaries
-  x = Math.max(0, Math.min(x, container.clientWidth - activeCube.clientWidth));
-  y = Math.max(0, Math.min(y, container.clientHeight - activeCube.clientHeight));
-
-  activeCube.style.left = x + "px";
-  activeCube.style.top = y + "px";
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
 });
 
-document.addEventListener("mouseup", () => {
-  if (activeCube) activeCube.style.zIndex = 1;
-  activeCube = null;
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; // scroll speed
+  slider.scrollLeft = scrollLeft - walk;
 });
